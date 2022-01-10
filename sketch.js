0eed3f57f815233227640259d9975eb982cb436a
Paulo Hilton <pahbaracho@gmail.com>
2022-01-10 18:01:31 -0300

    Criei as Caixas e o MouseDragged

--- sketch.js
+++ sketch.js
@@ -4,8 +4,10 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var box1, box2, box3,box4;
-var hero,monster,rope,ground;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10;
+var box11, box12, box13, box14, box15, box16, box17, box19, box20;
+var box21, box22, box23, box24, box25;
+var hero, monster, rope, ground;
 
 function preload() {
   bg = loadImage("Background IMG/gamingbackground2.png");
@@ -23,23 +25,54 @@ function setup() {
   monster = new Monster(1100,550,300);
 
   box1 = new Box(600, 100, 70, 70);
-  box2 = new Box(900, 100, 70, 70);
-  box3 = new Box(900, 100, 70, 70);
-  box4 = new Box(900, 100, 70, 70);
+  box2 = new Box(700, 170, 70, 70);
+  box3 = new Box(800, 240, 70, 70);
+  box4 = new Box(500, 310, 70, 70);
+  box5 = new Box(450, 380, 70, 70);
+  box6 = new Box(900, 450, 70, 70);
+  box7 = new Box(600, 530, 70, 70);
+  box8 = new Box(900, 100, 70, 70);
+  box9 = new Box(600, 100, 70, 70);
+  box11 = new Box(600, 100, 70, 70);
+  box12 = new Box(600, 100, 70, 70);
+  box13 = new Box(600, 100, 70, 70);
+  box14 = new Box(600, 100, 70, 70);
+  box15 = new Box(600, 100, 70, 70);
+  box23 = new Box(900, 100, 70, 70);
+  box24 = new Box(900, 100, 70, 70);
+  box25 = new Box(900, 100, 70, 70);
 
 }
 
 function draw() {
   background(bg);
   Engine.update(engine);
+  
   ground.display();
   box1.display();
   box2.display();
   box3.display();
   box4.display()
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display()
+  box9.display()
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box23.display()
+  box24.display()
+  box25.display()
 
   hero.display();
   rope.display();
   monster.display();
 
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body, {x: mouseX, y: mouseY});
 }
